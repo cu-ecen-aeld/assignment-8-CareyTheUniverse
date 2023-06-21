c78cf48f4607738fcddeffb667bad0c6f01e1a81
kjcarey <kjcarey@Ubuntu.myguest.virtualbox.org>
2023-06-20 20:16:51 -0500

    All test passed

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,20 +1,20 @@
 
 ##############################################################
 #
-# AESDCHAR_DEVICE
+# AESDCHAR
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_DEVICE = '55fa81b422ef34d62f233e4622486b07d332abd1'
+AESDCHAR_VERSION = '55fa81b422ef34d62f233e4622486b07d332abd1'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-CareyTheUniverse.git'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-CareyTheUniverse.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
 
-AESDCHAR_DEVICE_MODULE_SUBDIRS = aesd-char-driver
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 
 define AESDCHAR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
